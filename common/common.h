751a5cf77695897fbf01ef02a6bc3506977ab007
dkeenan7 <dkeenan7@f4d96251-2841-4e9e-997b-1d26c018c8b3>
2012-06-08 02:47:50 +0000

    Changed image addresses for normal 4K images #if IMAGE_START < $F000 -> #if IMAGE_START < $F800

--- common/common.h
+++ common/common.h
@@ -4,8 +4,8 @@
 #define		WATCHDOG	1			// True if watchdog timer is to be used (only turn off for debugging)
 									// Turning it off doesn't work because BSL will still clear and restart
 									// the watchdog timer on every call to ReadByte.
-#define		IMAGE_START	$F800		// Start of program image in flash memory. Always ends at $FFFF
+#define		IMAGE_START	$F000		// Start of program image in flash memory. Always ends at $FFFF
 //#define		BSL_IMAGE_START IMAGE_START	// Start of program image for the BSL
 			; The above is usually the same as IMAGE_START, but can be different if a transition
 			  ; image is required
-#define		BSL_IMAGE_START		$F000	// For 4K transition debugger
+#define		BSL_IMAGE_START		$F000	// For 4K debugger
