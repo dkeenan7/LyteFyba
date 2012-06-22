ce45841af3217c9ff3790d95d829965320c49b04
dkeenan7 <dkeenan7@f4d96251-2841-4e9e-997b-1d26c018c8b3>
2012-06-22 03:50:59 +0000

    Changed all projects to MSP430G2452 with Project/Options/Linker/Checksum Fill unused code memory turned OFF. Changed comment re BSL_IMAGE_START.

--- common/common.h
+++ common/common.h
@@ -5,7 +5,10 @@
 									// Turning it off doesn't work because BSL will still clear and restart
 									// the watchdog timer on every call to ReadByte.
 #define		IMAGE_START	$F000		// Start of program image in flash memory. Always ends at $FFFF
-//#define		BSL_IMAGE_START IMAGE_START	// Start of program image for the BSL
-			; The above is usually the same as IMAGE_START, but can be different if a transition
-			  ; image is required
-#define		BSL_IMAGE_START		$F000	// For 4K debugger
+									// Low flash may be used for data logging in future.
+
+; The address the BSL downloads to is usually the same as IMAGE_START,
+; but when making a transition between different download sizes, the debugger that does the
+; update to the new BSL will still need to be the old size, so it can be downloaded by the old BSL.
+#define		BSL_IMAGE_START IMAGE_START	// Where the BSL should put the images it downloads
+//#define		BSL_IMAGE_START		$E000	// Would be used temporarily while changing to a BSL that loads an 8K image
