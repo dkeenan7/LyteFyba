e0900e051ed8db71d8110d5c1124dfd6b6431a76
dkeenan7 <dkeenan7@f4d96251-2841-4e9e-997b-1d26c018c8b3>
2013-06-20 14:13:21 +0000

    Adding a second bootstrap loader in the last 512 byte segment of main-flash, to overcome limitations of the existing one in 256 byte info-flash. Work in progress.

--- common/common.h
+++ common/common.h
@@ -3,12 +3,13 @@
 #define		WATCHDOG	1			// True if watchdog timer is to be used (only turn off for debugging)
 									// Turning it off doesn't work because BSL will still clear and
 									// restart the watchdog timer on every call to ReadByte.
-#define		IMAGE_START	$F000		// Start of program image in flash memory. Ends at $FEFF
+#define		PROG_START	$F000		// Start of program image in flash memory. Ends at $FDFF
 									// Low flash may be used for data logging in future.
+#define		BSL2_START	$FE00		// Start of BSL2 image in flash memory. Ends at $FFFD
 
-; The address the BSL downloads to is usually the same as IMAGE_START,
+; The address BSL2 downloads to is usually the same as PROG_START,
 ; but when making a transition between different download sizes, the version of TestICal that does the
-; update to the new BSL will still need to be the old size, so it can be downloaded by the old BSL.
-#define		BSL_IMAGE_START IMAGE_START	// Where the BSL should put the images it downloads
-//#define		BSL_IMAGE_START		$E000	// Would be used temporarily while changing to a BSL that
-										//	loads an 8K image
+; update to the new BSL2 will still need to be the old size, so it can be downloaded by the old BSL2.
+#define		PROG_START_FOR_BSL PROG_START	// Where the BSL should put the images it downloads
+//#define		PROG_START_FOR_BSL		$E000	// Would be used temporarily while changing to a BSL
+										//	that loads an 8K image
