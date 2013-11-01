0b4d8a0ae32d8874ce4a114af60461c95057c3de
dkeenan7 <dkeenan7@users.noreply.github.com>
2013-11-01 06:20:54 +0000

    Receiving of BMUB status done by BMUA, not BMUB Renamed DC_BMUB_STRESS to DC_BMUB_STATUS

--- master/tritium/can.h
+++ master/tritium/can.h
@@ -99,7 +99,7 @@ void 					can_mod( unsigned char identifier, unsigned char mask, unsigned char d
 #define DC_POWER		2
 #define DC_RESET		3
 #define DC_SWITCH		5
-#define DC_BMUB_STRESS	6			// Send BMS worst stress from DCU B to A
+#define DC_BMUB_STATUS	6			// Send BMS stress (containing worst stress) from DCU B to A
 #define DC_BOOTLOAD		22
 
 
