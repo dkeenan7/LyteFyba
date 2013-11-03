226bb2fd2ff7d2ccc022476e6dd5f4b449166e69
dkeenan7 <dkeenan7@f4d96251-2841-4e9e-997b-1d26c018c8b3>
2013-11-03 12:54:52 +0000

    Restructured handleBMUstatusByte() for better flow and readability (without duplication). Untested. Renamed "bDcuB" to "bDCUb" as described here http://en.wikipedia.org/wiki/CamelCase#Criticism Changed gauge_2 to be a third simulated resistive output instead of a frequency output. Untested. Added sending of maxStress to gauge_2 (the oil pressure gauge) in handleBMUstatusByte(). Untested.

--- master/tritium/tri86.h
+++ master/tritium/tri86.h
@@ -144,7 +144,7 @@ void dint();
 extern volatile unsigned int events;
 extern volatile unsigned int chgr_events;		// Charger events
 extern volatile unsigned int bmu_events;		// BMU events
-extern bool bDcuB;								// True if DCU B; false if DCU A
+extern bool bDCUb;								// True if DCU B; false if DCU A
 extern unsigned char statusB;					// Status from DCU-B
 
 void fault() __attribute__ NOINLINE;			// Single flash the error LED
