50c08b961f95b1b6501fa0f8be7acae7d6af80cb
dkeenan7 <dkeenan7@users.noreply.github.com>
2014-09-13 07:59:43 +0000

    Added macros for multiple shifts rra2, rra3 ...rla2, rla3 ... to reduce clutter in the source code for the Insulation test command.

--- common/comMacros.h
+++ common/comMacros.h
@@ -4,23 +4,23 @@
 NCtoAllBits	MACRO	dest
 			subc	dest,dest	; Sets all bits to not carry
 			ENDM
-			
+
 CtoAllBits	MACRO	dest
 			subc	dest,dest	; Sets all bits to not carry
 			inv		dest		; Sets all bits to carry
 			ENDM
-			
+
 allBitsIfZ	MACRO	src,dest
 			and		src,src		; Sets carry if src is not zero
 			subc	dest,dest	; Sets all bits to not carry = zero
 			ENDM
-			
+
 allBitsIfNZ	MACRO	src,dest
 			and		src,src		; Sets carry if src is not zero
 			subc	dest,dest	; Sets all bits to not carry = zero
 			inv		dest		; Sets all bits to carry = not zero
 			ENDM
-			
+
 movBits		MACRO	src,mask,dest	; Trashes src
 			xor		dest,src	; Get one bits in src for bits that need to toggle
 			and		mask,src	; Mask so we only change those we want to change
@@ -32,3 +32,74 @@ movBits_B	MACRO	src,mask,dest	; Trashes src
 			and.b	mask,src	; Mask so we only change those we want to change
 			xor.b	src,dest	; Toggle bits as required
 			ENDM
+
+rra2		MACRO  dest
+			rra	dest
+			rra	dest
+			ENDM
+
+rra3		MACRO  dest
+			rra	dest
+			rra	dest
+			rra	dest
+			ENDM
+
+rra4		MACRO  dest
+			rra	dest
+			rra	dest
+			rra	dest
+			rra	dest
+			ENDM
+
+rra5		MACRO  dest
+			rra	dest
+			rra	dest
+			rra	dest
+			rra	dest
+			rra	dest
+			ENDM
+
+rra6		MACRO  dest
+			rra	dest
+			rra	dest
+			rra	dest
+			rra	dest
+			rra	dest
+			rra	dest
+			ENDM
+
+rla2		MACRO  dest
+			rla	dest
+			rla	dest
+			ENDM
+
+rla3		MACRO  dest
+			rla	dest
+			rla	dest
+			rla	dest
+			ENDM
+
+rla4		MACRO  dest
+			rla	dest
+			rla	dest
+			rla	dest
+			rla	dest
+			ENDM
+
+rla5		MACRO  dest
+			rla	dest
+			rla	dest
+			rla	dest
+			rla	dest
+			rla	dest
+			ENDM
+
+rla6		MACRO  dest
+			rla	dest
+			rla	dest
+			rla	dest
+			rla	dest
+			rla	dest
+			rla	dest
+			ENDM
+
