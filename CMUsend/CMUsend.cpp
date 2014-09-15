a22b4a23a375c3a22ca0ed017134c3ae3a84a369
dkeenan7 <dkeenan7@f4d96251-2841-4e9e-997b-1d26c018c8b3>
2014-09-15 01:49:40 +0000

    Rename BMUsend folder to CMUsend

---
+++ CMUsend/CMUsend.cpp
@@ -0,0 +1,232 @@
+// BMUsend.cpp : Defines the class behaviors for the application.
+//
+
+#include "stdafx.h"
+#include "BMUsend.h"
+#include "MainFrm.h"
+
+#include "BMUsendDoc.h"
+#include "BMUsendView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CBMUsendApp
+
+BEGIN_MESSAGE_MAP(CBMUsendApp, CWinApp)
+	ON_COMMAND(ID_APP_ABOUT,	&CBMUsendApp::OnAppAbout)
+	ON_COMMAND(ID_IMAGE_ALL1,	&CBMUsendApp::OnImageAll1)
+	ON_COMMAND(ID_IMAGE_PROGRAM,&CBMUsendApp::OnImageProgram)
+	ON_COMMAND(ID_IMAGE_BSL2,	&CBMUsendApp::OnImageBSL2)
+	ON_COMMAND(ID_IMAGE_BADSUM,	&CBMUsendApp::OnImageBadSum)
+END_MESSAGE_MAP()
+
+
+// CBMUsendApp construction
+
+CBMUsendApp::CBMUsendApp()
+	: m_password_sel(2)
+	, m_image_sel(1)
+	, m_bBadSum(0)
+	, m_start_off(0)
+	, m_total_len(0)
+	, m_len_to_send(0)
+	, m_nPortIndex(0)
+{
+}
+
+
+// The one and only CBMUsendApp object
+
+CBMUsendApp theApp;
+
+// CBMUsendApp initialization
+
+BOOL CBMUsendApp::InitInstance()
+{
+	// InitCommonControlsEx() is required on Windows XP if an application
+	// manifest specifies use of ComCtl32.dll version 6 or later to enable
+	// visual styles.  Otherwise, any window creation will fail.
+	INITCOMMONCONTROLSEX InitCtrls;
+	InitCtrls.dwSize = sizeof(InitCtrls);
+	// Set this to include all the common control classes you want to use
+	// in your application.
+	InitCtrls.dwICC = ICC_WIN95_CLASSES;
+	InitCommonControlsEx(&InitCtrls);
+
+	CWinApp::InitInstance();
+
+	// Standard initialization
+	// If you are not using these features and wish to reduce the size
+	// of your final executable, you should remove from the following
+	// the specific initialization routines you do not need
+	// Change the registry key under which our settings are stored
+	// TODO: You should modify this string to be something appropriate
+	// such as the name of your company or organization
+	SetRegistryKey(_T("Local AppWizard-Generated Applications"));
+	LoadStdProfileSettings(8);  // Load standard INI file options (including MRU)
+	// Register the application's document templates.  Document templates
+	//  serve as the connection between documents, frame windows and views
+	CSingleDocTemplate* pDocTemplate;
+	pDocTemplate = new CSingleDocTemplate(
+		IDR_MAINFRAME,
+		RUNTIME_CLASS(CBMUsendDoc),
+		RUNTIME_CLASS(CMainFrame),       // main SDI frame window
+		RUNTIME_CLASS(CBMUsendView));
+	if (!pDocTemplate)
+		return FALSE;
+	AddDocTemplate(pDocTemplate);
+
+	// Parse command line for standard shell commands, DDE, file open
+	CCommandLineInfo cmdInfo;
+	ParseCommandLine(cmdInfo);
+
+
+	// Dispatch commands specified on the command line.  Will return FALSE if
+	// app was launched with /RegServer, /Register, /Unregserver or /Unregister.
+	if (!ProcessShellCommand(cmdInfo))
+		return FALSE;
+
+	// The one and only window has been initialized, so show and update it
+	m_pMainWnd->ShowWindow(SW_SHOW);
+	m_pMainWnd->UpdateWindow();
+	// call DragAcceptFiles only if there's a suffix
+	//  In an SDI app, this should occur after ProcessShellCommand
+
+	m_bFileValid = false;
+	m_nProgress = 0;
+	_tcscpy_s(m_szShortName, _T("No file loaded"));
+	EnumSerialPorts(m_asiPorts, true);
+
+	UpdateMenu();
+
+	return TRUE;
+}
+
+
+
+// CAboutDlg dialog used for App About
+
+class CAboutDlg : public CDialog
+{
+public:
+	CAboutDlg();
+
+// Dialog Data
+	enum { IDD = IDD_ABOUTBOX };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+// Implementation
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialog(CAboutDlg::IDD)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
+END_MESSAGE_MAP()
+
+
+
+// App command to run the dialog
+void CBMUsendApp::OnAppAbout()
+{
+	CAboutDlg aboutDlg;
+	aboutDlg.DoModal();
+}
+
+
+// CBMUsendApp message handlers
+
+
+
+void CBMUsendApp::UpdateTitle()
+{
+	CString csPort;
+	if (m_asiPorts.GetCount() == 0)
+		csPort = _T("No port available");
+	else
+		csPort = m_asiPorts[m_nPortIndex].strFriendlyName;
+	m_pMainWnd->SetWindowText(CString(m_szShortName) + _T(" -> ") + csPort);
+}
+
+void CBMUsendApp::Adjust_start_and_len() {
+	// Calculate start offset and length to send based on the image selection. In the length to send, don't include
+	//	reset vector (never sent) but do include the checksum (not sent from the image)
+	switch (m_image_sel) {
+		case 1:									// Main program (TestICal or Monitor)
+			m_start_off = 0;					// Start at the beginning of the image
+			m_len_to_send = m_total_len - 512;	// Remove space for BSL2 (one flash segment = 512 bytes)
+			break;
+		case 2:									// BSL2 only
+			m_start_off = m_total_len - 512;	// Start 512 bytes from the end
+			m_len_to_send = 512-2;				// Send only the 512 bytes of BSL2, less reset vector
+			break;
+		case 3:									// All (both the above)
+			m_start_off = 0;
+			m_len_to_send = m_total_len - 2;	// Remove only reset vector
+			break;
+	}
+
+	m_pMainWnd->InvalidateRect(NULL);
+	m_pMainWnd->UpdateWindow();	// Force a paint of the number of bytes
+}
+
+void CBMUsendApp::UpdateMenu() {
+	CMenu* mmenu = m_pMainWnd->GetMenu();		// Whole menu
+	CMenu* imenu = mmenu->GetSubMenu(2);		// Image menu
+	if (theApp.m_bBadSum) {
+		imenu->CheckMenuItem(ID_IMAGE_BADSUM, MF_CHECKED);
+		imenu->CheckMenuItem(ID_IMAGE_ALL1, MF_UNCHECKED);
+		imenu->CheckMenuItem(ID_IMAGE_PROGRAM, MF_UNCHECKED);
+		imenu->CheckMenuItem(ID_IMAGE_BSL2, MF_UNCHECKED);
+	}
+	else {
+		imenu->CheckMenuItem(ID_IMAGE_BADSUM, MF_UNCHECKED);
+		if (theApp.m_image_sel == 3) {
+			imenu->CheckMenuItem(ID_IMAGE_ALL1,	MF_BYCOMMAND | ((theApp.m_password_sel == 1) ? MF_CHECKED : MF_UNCHECKED));
+		}
+		else {
+			imenu->CheckMenuItem(ID_IMAGE_ALL1,	MF_BYCOMMAND | MF_UNCHECKED);
+		}
+		imenu->CheckMenuItem(ID_IMAGE_PROGRAM,	MF_BYCOMMAND | ((theApp.m_image_sel == 1) ? MF_CHECKED : MF_UNCHECKED));
+		imenu->CheckMenuItem(ID_IMAGE_BSL2,		MF_BYCOMMAND | ((theApp.m_image_sel == 2) ? MF_CHECKED : MF_UNCHECKED));
+	}
+	Adjust_start_and_len();
+}
+
+void CBMUsendApp::OnImageAll1()	{
+	theApp.m_password_sel = 1;
+	theApp.m_image_sel = 3;
+	theApp.m_bBadSum = false;
+	UpdateMenu();
+}
+void CBMUsendApp::OnImageProgram()	{ 
+	theApp.m_password_sel = 2;
+	theApp.m_image_sel = 1;
+	theApp.m_bBadSum = false;
+	UpdateMenu();
+}
+void CBMUsendApp::OnImageBSL2()		{
+	theApp.m_password_sel = 1;
+	theApp.m_image_sel = 2;
+	theApp.m_bBadSum = false;
+	UpdateMenu();
+}
+void CBMUsendApp::OnImageBadSum()	{ 
+	theApp.m_password_sel = 2;
+	theApp.m_image_sel = 1;
+	theApp.m_bBadSum = true;
+	UpdateMenu();
+}
---
+++ CMUsend/CMUsendDoc.cpp
@@ -0,0 +1,542 @@
+// BMUsendDoc.cpp : implementation of the CBMUsendDoc class
+//
+
+#include "stdafx.h"
+#include "BMUsend.h"
+
+#include "BMUsendDoc.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CPortDlg dialog used for choosing a serial port
+
+class CPortDlg : public CDialog
+{
+public:
+	CPortDlg();
+
+// Dialog Data
+	enum { IDD = IDD_DLG_SELPORT };
+
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+// Implementation
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	CListBox m_listPorts;
+protected:
+	virtual void OnOK();
+
+	virtual BOOL OnInitDialog();
+
+public:
+	afx_msg void OnBnClickedRefresh();
+};
+
+CPortDlg::CPortDlg() : CDialog(CPortDlg::IDD)
+{
+}
+
+BOOL CPortDlg::OnInitDialog() 
+{
+	CDialog::OnInitDialog();
+
+	OnBnClickedRefresh();
+	return TRUE;
+}
+
+void CPortDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST_PORTS, m_listPorts);
+}
+
+void CPortDlg::OnOK()
+{
+	UpdateData();
+	theApp.m_nPortIndex = m_listPorts.GetCurSel();
+	theApp.UpdateTitle();
+	CDialog::OnOK();
+}
+
+void CPortDlg::OnBnClickedRefresh()
+{
+	EnumSerialPorts(theApp.m_asiPorts, true);
+	int n = (int)(theApp.m_asiPorts.GetCount());
+	m_listPorts.ResetContent();
+	for (int i=0; i < n; ++i)
+		m_listPorts.AddString(theApp.m_asiPorts[i].strFriendlyName);
+	m_listPorts.SetCurSel(theApp.m_nPortIndex);
+}
+
+
+BEGIN_MESSAGE_MAP(CPortDlg, CDialog)
+	ON_BN_CLICKED(ID_REFRESH, &CPortDlg::OnBnClickedRefresh)
+END_MESSAGE_MAP()
+
+//	//	//	//	//	//	//	//	//	//	//
+// CProgDlg dialog used Send Progress	//
+//	//	//	//	//	//	//	//	//	//	//
+
+class CProgDlg : public CDialog
+{
+public:
+	CProgDlg();
+
+// Dialog Data
+	enum { IDD = IDD_PROGRESS };
+
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+// Implementation
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	CProgressCtrl m_Bar;
+protected:
+
+	virtual BOOL OnInitDialog();
+
+public:
+	afx_msg void PostNcDestroy();
+public:
+	afx_msg void OnSetFocus(CWnd* pOldWnd);
+};
+
+CProgDlg::CProgDlg() : CDialog(CProgDlg::IDD)
+{
+}
+
+BOOL CProgDlg::OnInitDialog() 
+{
+	CDialog::OnInitDialog();
+
+	//m_Bar.SetRange(0, 2048);
+	return TRUE;
+}
+
+void CProgDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_PROGRESS, m_Bar);
+}
+
+BEGIN_MESSAGE_MAP(CProgDlg, CDialog)
+	ON_WM_SETFOCUS()
+END_MESSAGE_MAP()
+
+
+
+
+// CBMUsendDoc
+
+IMPLEMENT_DYNCREATE(CBMUsendDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CBMUsendDoc, CDocument)
+	ON_COMMAND(ID_FILE_OPEN, &CBMUsendDoc::OnFileOpen)
+	ON_UPDATE_COMMAND_UI(ID_SEND, &CBMUsendDoc::OnUpdateSend)
+	ON_COMMAND_RANGE(ID_FILE_MRU_FILE1, ID_FILE_MRU_FILE16, &CBMUsendDoc::OnFileMruFile)
+	ON_COMMAND(ID_SEND, &CBMUsendDoc::OnSend)
+	ON_COMMAND(ID_SETSERIAL, &CBMUsendDoc::OnSetserial)
+	ON_COMMAND(ID_FILE_SAVEAS, &CBMUsendDoc::OnFileSaveas)
+END_MESSAGE_MAP()
+
+
+// CBMUsendDoc construction/destruction
+
+CBMUsendDoc::CBMUsendDoc()
+{
+}
+
+CBMUsendDoc::~CBMUsendDoc()
+{
+}
+
+BOOL CBMUsendDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: add reinitialization code here
+	// (SDI documents will reuse this document)
+
+	return TRUE;
+}
+
+
+
+
+// CBMUsendDoc serialization
+
+void CBMUsendDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: add storing code here
+	}
+	else
+	{
+		// TODO: add loading code here
+	}
+}
+
+
+// CBMUsendDoc diagnostics
+
+#ifdef _DEBUG
+void CBMUsendDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CBMUsendDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CBMUsendDoc commands
+
+void CBMUsendDoc::OnFileOpen()
+{
+	OPENFILENAME ofn;
+	ZeroMemory( &ofn , sizeof( ofn));
+	ofn.lStructSize = sizeof ( ofn );
+	ofn.lpstrFilter = _T("Binary\0*.bin\0Hex\0*.hex\0All\0*.*");
+	ofn.lpstrFile = theApp.m_szFileName;
+	ofn.nMaxFile = sizeof( theApp.m_szFileName );
+	theApp.m_szFileName[0] = '\0';
+	ofn.lpstrFileTitle = theApp.m_szShortName;
+	ofn.nMaxFileTitle = sizeof( theApp.m_szShortName );
+	ofn.lpstrInitialDir = NULL ;
+	ofn.hwndOwner = NULL ;
+	ofn.nFilterIndex =1;		// First filter, i.e. hex
+	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST ;
+ 
+	if (GetOpenFileName(&ofn))
+	{
+		theApp.AddToRecentFileList( theApp.m_szFileName );
+		ReadFile();
+	}
+}
+
+
+void CBMUsendDoc::OnUpdateSend(CCmdUI *pCmdUI)
+{
+	// TODO: Add your command update UI handler code here
+	pCmdUI->Enable(theApp.m_bFileValid);
+}
+
+void CBMUsendDoc::OnFileMruFile(UINT uiMsgId)
+{
+	// TODO: Add your command handler code here
+	LPCTSTR pPathlessName = _tcsrchr((*theApp.GetRecentFileList())[uiMsgId - ID_FILE_MRU_FILE1], '\\');
+	pPathlessName++;			// Skip the \\ (possibly wide) character
+	_tcscpy_s(theApp.m_szShortName, pPathlessName);
+	UpdateAllViews(NULL);
+	_tcscpy_s(theApp.m_szFileName, (*theApp.GetRecentFileList())[uiMsgId - ID_FILE_MRU_FILE1]);
+	ReadFile();
+}
+
+// Disgusting globals
+static unsigned char sum;
+static unsigned int add;
+HANDLE hComm;
+
+
+static bool readColon(CFile& f) {
+	char ch;
+	do {
+		if (f.Read(&ch, 1) == 0)
+			return false;
+	} while (ch != ':');
+	return true;
+}
+
+static unsigned int readHexNibble(CFile& f) {
+    char c;
+	f.Read(&c, 1);
+    int r;
+    if (c >= '0') {
+        r = c - '0';
+        if (r > 9)
+            r -= 'A'-1-'9';
+        if (r <= 0xF)
+            return r;
+    }
+
+    TCHAR msg[32];
+	_stprintf_s(msg, sizeof(msg), _T("Unexpected char '%c' when reading hex, last address = %X"), c, add);
+	MessageBox(theApp.m_pMainWnd->m_hWnd, msg, _T("Error"), MB_ICONSTOP);
+    exit(1);
+    return -1;
+}
+
+static unsigned int readHexByte(CFile& f) {
+    unsigned int r = (readHexNibble(f) << 4) + readHexNibble(f);
+    sum += r;
+    return r;
+}
+
+static unsigned int readHexWord(CFile& f) {
+    return (readHexByte(f) << 8) + readHexByte(f);
+}
+
+void CBMUsendDoc::ReadFile()
+{
+	theApp.UpdateTitle();
+	CFile f(theApp.m_szFileName, CFile::modeRead);
+	unsigned int len, typ, checksum;
+	unsigned int u;
+	if (_tcscmp(theApp.m_szShortName + _tcslen(theApp.m_szShortName)-4, _T(".hex")) == 0) {
+		theApp.m_bFileValid = false;
+		theApp.m_total_len = 0;
+		m_first_addr = (unsigned int) -1;
+
+		memset(m_fileBuf, '\xFF', 8192);
+
+		do {
+			if (!readColon(f))
+				break;
+			sum = 0;
+			len = readHexByte(f);
+			// m_total_len += len;		// No! This doesn't work when there are overlaps or gaps
+			add = readHexWord(f);
+			typ = readHexByte(f);
+			if (typ == 1)
+				break;
+			if (typ > 0) {
+				TCHAR msg[40];
+				_stprintf_s(msg, _T("Unexpected record type %X at address %X"), typ, add);
+				MessageBox(theApp.m_pMainWnd->m_hWnd, msg, _T("Error"), MB_ICONSTOP);
+				f.Close();
+				return;
+			}
+			if (add < 0xE000)		/* Could be initialisation in RAM, for example */
+				continue;           /* Repeat the loop, looking for the colon on the next line */
+			if (m_first_addr == (unsigned int) -1)
+				/* Assume that the first address read is the start of the image */
+				m_first_addr = add;
+			unsigned char* p = m_fileBuf + add - m_first_addr;
+			for (u=0; u < len; ++u) {
+				*p++ = readHexByte(f);
+			}
+			checksum = readHexByte(f);
+			if (sum & 0xFF) {
+				TCHAR msg[40];
+				_stprintf_s(msg, _T("Bad checksum %X expected %X"), checksum, 0-(sum-checksum) & 0xFF);
+				MessageBox(theApp.m_pMainWnd->m_hWnd, msg, _T("Error"), MB_ICONSTOP);
+				f.Close();
+				return;
+			}
+			add += len;
+		} while (1);
+		theApp.m_total_len = 0xFFFF+1 - m_first_addr;		// Assume last byte will load at 0xFFFF (MSB of reset vector)
+	} else
+	{	// Not a hex file; assume binary
+		// Close it and open it again in binary mode
+		f.Close();
+		f.Open(theApp.m_szFileName, CFile::modeRead | CFile::typeBinary);
+		theApp.m_total_len = (unsigned int) f.GetLength();
+		f.Read(m_fileBuf, theApp.m_total_len);
+	}
+	f.Close();
+	if (theApp.m_total_len)
+		theApp.m_bFileValid = true;		// Enables the ID_SEND green arrow
+
+	theApp.Adjust_start_and_len();
+
+}
+
+
+static void writeByte(unsigned char* p) {
+	OVERLAPPED osWrite = {0};
+
+	// Create this write operation's OVERLAPPED structure's hEvent.
+	osWrite.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	if (osWrite.hEvent == NULL) {
+		theApp.m_pMainWnd->MessageBox(_T("Error creating overlapped event handle"), _T("Internal comms error"),
+			MB_ICONSTOP);
+		return;
+	}
+
+	// Issue write.
+	DWORD dwWritten;
+	if (!WriteFile(hComm, p, 1, /*NULL*/&dwWritten, &osWrite)) {
+		if (GetLastError() != ERROR_IO_PENDING) {
+		theApp.m_pMainWnd->MessageBox(_T("WriteFile failed, but isn't delayed."), _T("Internal comms error"),
+			MB_ICONSTOP);
+			return;
+		}
+	}
+	CloseHandle(osWrite.hEvent);
+}
+
+void CBMUsendDoc::OnSend()
+{
+	ReadFile();					// In case sending the same file with changes
+	// Open comm port
+	if (theApp.m_asiPorts.GetCount() == 0)
+	{
+		theApp.m_pMainWnd->MessageBox(_T("There were no communications ports found"), _T("Comms error"), MB_ICONSTOP);
+		return;
+	}
+
+	CString& csFriendlyName = theApp.m_asiPorts[theApp.m_nPortIndex].strFriendlyName;
+	int idxLparen = csFriendlyName.ReverseFind(_T('('));
+	if ((idxLparen != -1) &&
+		csFriendlyName[idxLparen+1] == 'C' && 
+		csFriendlyName[idxLparen+2] == 'O' && 
+		csFriendlyName[idxLparen+3] == 'M')
+	{
+		int idxRparen = csFriendlyName.ReverseFind(_T(')'));
+		CString csNumber(csFriendlyName.Mid(idxLparen+4, idxRparen - (idxLparen+4)));
+		int comPortNumber = _tstoi(csNumber.GetBuffer());
+		TCHAR sName[32];
+		COMMCONFIG  lpCC;
+		_stprintf_s(sName, sizeof(sName)/sizeof(TCHAR), _T("\\\\.\\COM%d"), comPortNumber);
+		hComm = CreateFile(sName,
+			GENERIC_WRITE,
+			0,
+			0,
+			OPEN_EXISTING,
+			FILE_ATTRIBUTE_NORMAL,
+			0);
+		if (hComm == INVALID_HANDLE_VALUE) {
+			TCHAR szMsg[64];
+			_stprintf_s(szMsg, sizeof(szMsg)/sizeof(TCHAR), _T("Error opening port COM%d\nMay be in use by another application"),
+				comPortNumber);
+			theApp.m_pMainWnd->MessageBox(szMsg, _T("Comms error"), MB_ICONSTOP);
+			return;
+		}
+		GetCommState( hComm, &lpCC.dcb);
+
+		 /* Initialisation of parameters */
+		lpCC.dcb.BaudRate = CBR_9600;
+		//lpCC.dcb.BaudRate = CBR_19200;
+		lpCC.dcb.ByteSize = 8;
+		lpCC.dcb.StopBits = ONESTOPBIT;
+		lpCC.dcb.Parity = NOPARITY;
+		lpCC.dcb.fDtrControl = DTR_CONTROL_ENABLE;		// Raise DTR when connected
+		lpCC.dcb.fRtsControl = RTS_CONTROL_DISABLE;
+		SetCommState(hComm, &lpCC.dcb );
+	}
+	else {
+		theApp.m_pMainWnd->MessageBox(CString(_T("Could not parse comm port name from \"")) + csFriendlyName, _T("Parse error"),
+			MB_ICONSTOP);
+		return;
+	}
+
+	CProgDlg* pProg = new CProgDlg();
+	pProg->Create(CProgDlg::IDD);
+	pProg->ShowWindow(SW_SHOW);
+	pProg->m_Bar.SetRange(0, theApp.m_len_to_send);
+
+
+    /* Now send this image to the BMUs */
+    unsigned int i, u;
+
+    /* Write the prefix (escape and the 4-character password) */
+	unsigned char pfx[5];
+	if (theApp.m_password_sel == 1)
+		memcpy(pfx, "\x1B\x03\x02\x01\x00", 5);
+	else
+		memcpy(pfx, "\x1B\x07\x06\x05\x04", 5);
+    for (i=0; i < 5; ++i) {
+        writeByte(pfx+i);
+		Sleep(1+1+1);					// Delay for send, echo, and "rounding"/safety
+    }
+
+    /* Allow time for segment erases (approximately 15 ms per segment) */
+	/* Note that m_len_to_send is sometimes 2 short of the real length, because of the reset vector */
+	/* Be conservative and use 21 ms per segment erase */
+	Sleep((((theApp.m_len_to_send + 2) / 512) * 21) +1);
+    // Send the appropriate number of bytes of the binary image (excluding last byte, to be replaced with the checksum)
+	sum = 0;							// Checksum
+    for (i=0, u = theApp.m_start_off; i < theApp.m_len_to_send-1; ++i, ++u) {
+        writeByte(m_fileBuf+u);         // Write one byte
+		sum ^= m_fileBuf[u];			// Update checksum
+        Sleep(2+1);						// Time to transmit, echo, and flash write (~ 0.2 ms); the 0.2 can be
+										//	part of the +1, which is because each bit is more like 1.04 ms, and
+										//	in case the clock is slow
+		if ((u & 0x3F) == 0x3F) {
+			theApp.m_nProgress = u;
+			pProg->SetFocus();			// Use the focus message to update the progress bar
+		}
+    }
+	// Finally send the checksum byte in place of the very last byte (either just before the reset vector, or right at the end
+	//	of the main program, ust before BSL2
+	// Sometimes we want to deliberately send a bad checksum
+	if (theApp.m_bBadSum)
+	{
+		sum ^= 0xFF;					// Invert checksum
+		writeByte(&sum);
+		sum ^= 0xFF;					// Correct checksum by re-inverting
+	}
+	else
+		writeByte(&sum);
+
+	theApp.m_nProgress = theApp.m_len_to_send;
+	pProg->SetFocus();
+
+	CloseHandle(hComm);
+
+}
+
+
+
+void CBMUsendDoc::OnSetserial()
+{
+	CPortDlg portDlg;
+	portDlg.DoModal();
+}
+
+
+void CProgDlg::PostNcDestroy()
+{
+	CDialog::PostNcDestroy();
+	delete this;
+
+}
+
+void CProgDlg::OnSetFocus(CWnd* pOldWnd)
+{
+	m_Bar.SetPos(theApp.m_nProgress);
+	CDialog::OnSetFocus(pOldWnd);
+	ShowWindow(SW_SHOW);
+}
+
+void CBMUsendDoc::OnFileSaveas()
+{
+	OPENFILENAME ofn;
+	ZeroMemory( &ofn , sizeof( ofn));
+	ofn.lStructSize = sizeof ( ofn );
+	ofn.lpstrFilter = _T("Bin\0*.bin\0All\0*.*");
+	ofn.lpstrFile = theApp.m_szFileName;
+	ofn.nMaxFile = sizeof(theApp.m_szFileName) / sizeof(TCHAR);
+	theApp.m_szFileName[0] = '\0';
+	ofn.lpstrFileTitle = theApp.m_szShortName;
+	ofn.nMaxFileTitle = sizeof(theApp.m_szShortName) / sizeof(TCHAR);
+	ofn.lpstrInitialDir = NULL ;
+	ofn.hwndOwner = NULL ;
+	ofn.nFilterIndex =1;		// First filter, i.e. hex
+	ofn.Flags = OFN_OVERWRITEPROMPT | OFN_SHOWHELP; //OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST ;
+ 
+	if (GetSaveFileName(&ofn))
+	{
+		CFile f(ofn.lpstrFile, CFile::modeCreate | CFile::modeWrite | CFile::typeBinary);
+		f.Write(m_fileBuf, theApp.m_total_len);
+		f.Close();
+	}
+	
+}
+
