39e33e316e396a8e1c90e4eb4a44a1f82e79a7d5
dkeenan7 <dkeenan7@f4d96251-2841-4e9e-997b-1d26c018c8b3>
2015-01-11 11:05:41 +0000

    Moved Delay130ms to after end of main loop Fixes 64 kB assembled binary problem

--- common/common.h
+++ common/common.h
@@ -29,14 +29,6 @@ RELAYp		EQU		1<<7			; IMU relay for HazV+ test on P2.7
 //#define		PROG_START_FOR_BSL		$E000	// Would be used temporarily while changing to a BSL that
 										//	loads a different sized image to the one it is contained in.
 										//	The password may need changing too, in both BSL2 and Monitor.
-; Called from multiple places
-Delay130ms:
-		_DO		#0,R11					; 65536 * 8 / 4 MHz = 131 ms delay
-
-			ClearWatchdog
-
-		_LOOP	R11						; (3) dec R11, jnz
-		ret
 
 
 			ORG		$10F8
