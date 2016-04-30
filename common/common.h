3acac54db9d75725575c085206675e6d4f84e9a1
Dave Keenan <dkeenan7@users.noreply.github.com>
2016-04-30 18:56:07 +1000

    Changed definitions in common.h to agree with new schematic Cont -> Ctor in many names (source code and BMU schematic), to avoid confusion with "contact"

--- common/common.h
+++ common/common.h
@@ -11,19 +11,12 @@ VrefP		EQU		1<<4			; Analog reference output on P1.4
 ArrayV_BoltVPl EQU	1<<5			; Analog input on P1.5
 ShuntV_BoltVMi EQU	1<<6			; Analog input on P1.6
 BatV_StrapVPl EQU	1<<7			; Analog input on P1.7
-TxMiPortDIR	EQU		P1DIR			; Transmit- to CMUs output is on port 1
-TxMiPortSEL	EQU		P1SEL
-TxMiPortSEL2 EQU	P1SEL2
-TxMiPortOUT	EQU		P1OUT
-PiezoPortDIR EQU	P1DIR			; Piezo output is on port 1
-PiezoPortSEL EQU	P1SEL
-PiezoPortOUT EQU	P1OUT
 
 ; Port 2 bit masks
 RelayM_Byp	EQU		1<<0			; IMU relay for HazV- test (BMU) on P2.0
-RelayP_Ir57k6 EQU	1<<1			; IMU relay for HazV+ test (BMU), 57.6 kHz IR carrier (CMU) on P2.1
-PreI		EQU		1<<2			; Precharge contactor auxiliary contact input (BMU)
-TxMiScu		EQU		1<<3			; Transmit- to SCU (TA1.0 output) on P2.3
+RelayP_57k6 EQU		1<<1			; IMU relay for HazV+ test (BMU), 57.6 kHz IR carrier (CMU) on P2.1
+TxMiScu		EQU		1<<2			; Transmit- to SCU (TA1.0 output) on P2.3
+PreI		EQU		1<<3			; Precharge contactor auxiliary contact input (BMU)
 RxScu		EQU		1<<4			; Receive from SCU (TA1.2 input) on P2.4
 ErrLed		EQU		1<<5			; Error LED (red) on P2.5
 Xin			EQU		1<<6			; Watch crystal in on P2.6
@@ -33,18 +26,18 @@ BypPortDIR	EQU		P2DIR			; Bypass MOSFET output on port 2
 BypPortSEL	EQU		P2SEL
 BypPortOUT	EQU		P2OUT
 RelayM		EQU		RelayM_Byp
-RelayP		EQU		RelayP_Ir57k6
+RelayP		EQU		RelayP_57k6
 
 ; Port 3 bit masks. BMU only. No change between rev <blank> and rev 4.
 RxChg		EQU		1<<0			; Receive from charger (TA0.2 input) on P3.0
-PreCont		EQU		1<<1			; Precharge contactor output on P3.1
-BatCont		EQU		1<<2			; Battery contactor output  P3.2
-ChgCont		EQU		1<<3			; Charge sources contactor output  P3.3
+PreCtor		EQU		1<<1			; Precharge contactor output on P3.1
+BatCtor		EQU		1<<2			; Battery contactor output  P3.2
+ChgCtor		EQU		1<<3			; Charge sources contactor output  P3.3
 TxMiChg		EQU		1<<4			; Transmit- to charger (TA0.0 output) on P3.4
-DisCont		EQU		1<<5			; Discharge or discretionary loads contactor output on P3.5
-NrmCont		EQU		1<<6			; Normal loads contactor output on P3.6
+DisCtor		EQU		1<<5			; Discharge or discretionary loads contactor output on P3.5
+NrmCtor		EQU		1<<6			; Normal loads contactor output on P3.6
 BatI		EQU		1<<7			; Battery contactor auxiliary contact input
-SocMeter	EQU		NrmCont			; PWM output for SoC meter (monolith only)
+SocMeter	EQU		NrmCtor			; PWM output for SoC meter (monolith only)
 SocPortDIR	EQU		P3DIR			; Soc meter PWM output is on port 3
 SocPortSEL	EQU		P3SEL
 SocPortOUT	EQU		P3OUT
