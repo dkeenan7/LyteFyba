90d48c4bf2897dd3a43d0cb9a8960aeed8540bfc
Dave Keenan <dkeenan7@users.noreply.github.com>
2016-12-15 01:11:34 +1000

    Fixed problems with the pre-build batch file for getting the SVN revision numbers to embed in the code and be returned by the # command. It now assumes SubWCRev.exe is in "%SystemDrive%\Program Files"\TortoiseSVN\bin\. It now takes the project directory as a parameter (%1).

--- common/GetSVNrevs.bat
+++ common/GetSVNrevs.bat
@@ -1,8 +1,7 @@
 @echo off
-REM This batch file runs from the trunk or rev61 or rev58 folder
-REM NOTE: SubWCRev.exe must be in the path; usually c:\Program Files\TortoiseSVN\bin
+REM This batch file takes the project folder (e.g. ...\trunk\monolith or ...\rev61\TestICal) as a parameter (%1)
+REM NOTE: SubWCRev.exe is assumed to be in C:\Program Files\TortoiseSVN\bin
 REM First: SVN revision for the whole repository branch/trunk
-SubWCRev.exe . common\GetSVNrev_in.h common\SVNrev.h
+"%SystemDrive%\Program Files"\TortoiseSVN\bin\SubWCRev.exe %1 %1\..\common\GetSVNrev_in.h %1\..\common\SVNrev.h
 REM Next: SVN revision specifically for BSL2.s43 (since it can be troublesome)
-SubWCRev.exe common\BSL2.s43 common\GetBSLrev_in.h common\BSLrev.h
-
+"%SystemDrive%\Program Files"\TortoiseSVN\bin\SubWCRev.exe %1\..\common\BSL2.s43 %1\..\common\GetBSLrev_in.h %1\..\common\BSLrev.h
