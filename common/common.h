4baab0559fb55a9a4e7f91ce704c0f3ad2909cf6
Dave Keenan <dkeenan7@users.noreply.github.com>
2017-01-30 19:35:30 +1000

    Battery capacity and cell internal resistance are now stored in info-flash with the calibration values Use stored battery capacity for 'g' command Added 'c' (capacity) and 'r' (cell resistance) definitions for calibrate read and write commands

--- common/common.h
+++ common/common.h
@@ -133,11 +133,14 @@ NumSamples	EQU		16				; Number of ADC over-samples (typ. 4 or 16)
 										//	loads a different sized image to the one it is contained in.
 										//	The password may need changing too, in both BSL2 and Monitor.
 
-			ORG		$1010			; Stay away from manufacturer supplied data
-	#define		CAL_IN_SEG_A	0
+			ORG		$100A			; Stay away from manufacturer supplied data
 			; Calibration data
 DATAVERS		EQU		6			; This is version 6 of the CMU info-flash data structure
+
 infoDataStart						; Used when copying between ram and info-flash
+infoCapacity	ds		2			; Battery capacity in tenths of an amp-hour
+infoCellRes		ds		2			; High temperature cell internal resistance in micro-ohms
+infoBoltPlCal	ds		2			; Not used here; retain for data compatibility
 infoVoltCal		ds		2			; Voltage scale calibration word
 infoTempCal		ds		1			; Temperature offset calibration for internal sensor
 infoLinkCal		ds		1			; Link voltage offset calibration data
