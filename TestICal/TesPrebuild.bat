06af7747f13ac8356b4d00cdb52dd57b16b144b9
Dave Keenan <dkeenan7@users.noreply.github.com>
2017-02-04 00:44:37 +1000

    Fixed some bugs, but Bootstrap loading is still not working. Made the bootstrap loader no longer a macro, so you can debug it properly with JTAG. Hoorah!

---
+++ TestICal/TesPrebuild.bat
@@ -0,0 +1,10 @@
+@echo off
+REM This batch file does the prebuild actions for the TestICal project
+REM It takes the project folder (e.g. ...\trunk\TestICal) as a parameter (%1)
+REM First create the file BSL2Macro.s43 by wrapping a macro definition around BSL2.s43
+REM This is necessary because IAR will not allow #includes inside macro definitions
+type %1\BSL2Macro.h %1\..\common\BSL2.s43 %1\BSL2Endm.h > %1\BSL2Macro.s43
+REM Now pass control to the batch file that creates the two files:
+REM SVNrev.h with the SVN revision for the whole repository and 
+REM ..\common\BSLrev.h with the SVN revision specifically for BSL2.s43
+%1\..\common\GetSVNrevs.bat %1
