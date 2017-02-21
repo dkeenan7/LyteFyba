e38ec5c131680df0ae3b8aca0c18ba5b52578763
Dave Keenan <dkeenan7@users.noreply.github.com>
2017-02-21 18:18:12 +1000

    In the trunk software, finally got bootstrap-loading to work without dropping out contactors and thereby losing power, thanks to much help from Dr Van Emmerik. Hoorah! Moved ErrorFlash routine to the BSL and added a BSL entry point for it. Changed BSL to disable all interrupts except the FLL before erasing the main program, which contains their ISRs. In the 'b' (update BSL) command, disabled all interrupts while the interrupt vectors are being erased and rewritten. In the BSL, corrected the logic and comments for the saving and checking of the reasons for a reset. Used a spare bit in IFG1 to indicate if it was a pseudo-reset due to a bootstrap-load. In monolith, raised the OT_ZERO threshold from 32degC to 33degC to agree with the recent change to rev61 monolith.

--- TestICal/BSL2Macro.h
+++ TestICal/BSL2Macro.h
@@ -1,12 +1,12 @@
 ; The start of a macro wrapper for the inactive copy of the bootstrap-loader and interrupt vectors
 ; as carried by TestICal for use by the 'b' (bootstrap-loader update) command.
-; The whole Macro is assembled by the following prebuild Windows command 
+; The whole Macro is assembled by the following prebuild Windows command
 ; type BSL2Macro.h BSL2.s43 BSL2Endm.h > BSL2Macro.s43
 ; This is needed because IAR doesn't allow #includes inside macros
 
-BSL2MACRO	MACRO	jBSL, jBSLErase, jReadByte, jWriteByte, jBslRevision, jWriteBreak
+BSL2MACRO	MACRO	jBSL, jBSLErase, jReadByte, jWriteByte, jBslRevision, jWriteBreak, jErrorFlash
 	LOCAL	BSL, BSLErase, ReadByte, WriteByte, BslRevision
-	LOCAL	MckPerFTGck, TestNoInterp2, WriteBreak
+	LOCAL	MckPerFTGck, TestNoInterp2, WriteBreak, ErrorFlash
 	LOCAL	freeSpaceBSL2, BSL2presence, BSL2checksum
-	LOCAL	ScuBaudRate, ScuBitCycles, ScuBitCyc1_5, TA0FllIsr
+	LOCAL	ScuBaudRate, ScuBitCycles, ScuBitCyc1_5, TA0FllIsr, BSLFG
 
