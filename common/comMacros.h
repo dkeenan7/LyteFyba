3020d03a1a563d674286bb90ae23f0457bab8671
Dave Keenan <dkeenan7@users.noreply.github.com>
2017-06-16 18:36:27 +1000

    Monolith command does only 'r', 'c', 'i' subcommands Added monolith 'ic (ident) and 'nc commands (zero current shunt) Enabled or_w and or_b macros MonoUpdFlash -> UpdFlash Commented out monolith "Charging" command (no longer used) Added 'dr command to read data version (monolith and TestICal) TestICal Ad -> 'ac Allow recovery of a CMU that has all its info-flash accidentally erased

--- common/comMacros.h
+++ common/comMacros.h
@@ -163,11 +163,9 @@ rla8_l		MACRO	hi,lo
 			swpb	lo
 			ENDM
 
-#if 0		// ? Mot an allowed label?
-or			MACRO	src,dest
+or_w		MACRO	src,dest
 			bis		src,dest			; Badly named mnemonic at times
 			ENDM
 or_b		MACRO	src,dest
 			bis.b	src,dest			; Badly named mnemonic at times
 			ENDM
-#endif
