ec93897182f2508645babeff288439a977ba92a1
Dave Keenan <dkeenan7@users.noreply.github.com>
2017-09-22 19:19:56 +1000

    Fixed bugs in trunk Monitor that have accumulated through many changes since rev58 and through neglect of proper testing. The most serious was an excess push of the cell voltage (without matching pop). Changes to trunk Monolith are cosmetic only, to minimise the differences between it and Monitor. Change to trunk TestICal is one comment corrected.

--- TestICal/TesPrebuild.bat
+++ TestICal/TesPrebuild.bat
@@ -4,7 +4,5 @@ REM It takes the project folder (e.g. ...\trunk\TestICal) as a parameter (%1)
 REM First create the file BSL2Macro.s43 by wrapping a macro definition around BSL2.s43
 REM This is necessary because IAR will not allow #includes inside macro definitions
 type %1\BSL2Macro.h %1\..\common\BSL2.s43 %1\BSL2Endm.h > %1\BSL2Macro.s43
-REM Now pass control to the batch file that creates the two files:
-REM SVNrev.h with the SVN revision for the whole repository and 
-REM ..\common\BSLrev.h with the SVN revision specifically for BSL2.s43
+REM Now pass control to the batch file that creates the files with the SVN revision numbers
 %1\..\common\GetSVNrevs.bat %1
